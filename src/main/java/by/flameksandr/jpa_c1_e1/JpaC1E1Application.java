f4aa7a3de1ab2e7d1a1e6ae2976a1cdc9b526b4a
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-10-22 17:51:36 +0300

    configuration with file CustomPersistenceUnitInfo, add HikariCP

--- src/main/java/by/flameksandr/jpa_c1_e1/JpaC1E1Application.java
+++ src/main/java/by/flameksandr/jpa_c1_e1/JpaC1E1Application.java
@@ -2,23 +2,28 @@ package by.flameksandr.jpa_c1_e1;
 
 
 import by.flameksandr.jpa_c1_e1.entities.Product;
+import by.flameksandr.jpa_c1_e1.persistance.CustomPersistenceUnitInfo;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.Persistence;
+import org.hibernate.jpa.HibernatePersistenceProvider;
+
+import java.util.HashMap;
 
 public class JpaC1E1Application {
 
     public static void main(String[] args) {
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("my-persistence-unit");
+        EntityManagerFactory emf = new HibernatePersistenceProvider()
+                .createContainerEntityManagerFactory(new CustomPersistenceUnitInfo(),
+                        new HashMap());
+//        EntityManagerFactory emf = Persistence.createEntityManagerFactory("my-persistence-unit");
 
         EntityManager em = emf.createEntityManager();
-
 
         try {
             em.getTransaction().begin();
             Product product = new Product();
-            product.setId(1);
-            product.setName("Beer");
+            product.setId(2);
+            product.setName("Chocolate");
             em.persist(product);
             em.getTransaction().commit();
         } finally {
---
+++ src/main/java/by/flameksandr/jpa_c1_e1/persistance/CustomPersistenceUnitInfo.java
@@ -0,0 +1,104 @@
+package by.flameksandr.jpa_c1_e1.persistance;
+
+import com.zaxxer.hikari.HikariDataSource;
+import jakarta.persistence.SharedCacheMode;
+import jakarta.persistence.ValidationMode;
+import jakarta.persistence.spi.ClassTransformer;
+import jakarta.persistence.spi.PersistenceUnitInfo;
+import jakarta.persistence.spi.PersistenceUnitTransactionType;
+
+import javax.sql.DataSource;
+import java.net.URL;
+import java.util.List;
+import java.util.Properties;
+
+public class CustomPersistenceUnitInfo implements PersistenceUnitInfo {
+    @Override
+    public String getPersistenceUnitName() {
+        return "my-persistence-unit";
+    }
+
+    @Override
+    public String getPersistenceProviderClassName() {
+        return "org.hibernate.jpa.HibernatePersistenceProvider";
+    }
+
+    @Override
+    public PersistenceUnitTransactionType getTransactionType() {
+        return PersistenceUnitTransactionType.RESOURCE_LOCAL;
+    }
+
+    @Override
+    public DataSource getJtaDataSource() {
+        HikariDataSource ds = new HikariDataSource();
+        ds.setJdbcUrl("jdbc:mysql://localhost/demo");
+        ds.setUsername("root");
+        ds.setPassword("password");
+        return ds;
+    }
+
+    @Override
+    public DataSource getNonJtaDataSource() {
+        return null;
+    }
+
+    @Override
+    public List<String> getMappingFileNames() {
+        return null;
+    }
+
+    @Override
+    public List<URL> getJarFileUrls() {
+        return null;
+    }
+
+    @Override
+    public URL getPersistenceUnitRootUrl() {
+        return null;
+    }
+
+    @Override
+    public List<String> getManagedClassNames() {
+        return List.of("by.flameksandr.jpa_c1_e1.entities.Product");
+    }
+
+    @Override
+    public boolean excludeUnlistedClasses() {
+        return false;
+    }
+
+    @Override
+    public SharedCacheMode getSharedCacheMode() {
+        return null;
+    }
+
+    @Override
+    public ValidationMode getValidationMode() {
+        return null;
+    }
+
+    @Override
+    public Properties getProperties() {
+        return null;
+    }
+
+    @Override
+    public String getPersistenceXMLSchemaVersion() {
+        return null;
+    }
+
+    @Override
+    public ClassLoader getClassLoader() {
+        return null;
+    }
+
+    @Override
+    public void addTransformer(ClassTransformer classTransformer) {
+
+    }
+
+    @Override
+    public ClassLoader getNewTempClassLoader() {
+        return null;
+    }
+}
