67263b50ee6df1bdf8389fd8027a25258bf06f98
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-10-22 19:36:19 +0300

    refactoring

--- src/main/java/by/flameksandr/jpa_c1_e1/JpaC1E1Application.java
+++ src/main/java/by/flameksandr/jpa_c1_e1/JpaC1E1Application.java
@@ -16,7 +16,7 @@ public class JpaC1E1Application {
         String puName = "pu-name";
         Map<String, String> props = new HashMap<>();
         props.put("hibernate.show_sql", "true");
-        props.put("hibernate.hbm2ddl.auto", "create");
+        props.put("hibernate.hbm2ddl.auto", "update");
 
         EntityManagerFactory emf = new HibernatePersistenceProvider()
                 .createContainerEntityManagerFactory(new CustomPersistenceUnitInfo(puName),
@@ -27,9 +27,13 @@ public class JpaC1E1Application {
         try {
             em.getTransaction().begin();
 
-//            Employee employee = em.find(Employee.class, 1);
-//            employee.setName("Aleksandr Philimonov");
-//            System.out.println(employee);
+//            em.persist();       -> Adding an entity in the context
+//            em.find();          -> Finds by PK. Get from DB and add it to the context if it doesn't already exist
+//            em.remove();        -> Marking entity for removal
+//            em.merge();         -> Merges an entity from outside the context to the context
+//            em.refresh();       -> Mirror the context from the adtabase
+//            em.detach();        -> Taking the entity out of the context
+//            em.getReference()   ->;
 
             em.getTransaction().commit();
         } finally {
