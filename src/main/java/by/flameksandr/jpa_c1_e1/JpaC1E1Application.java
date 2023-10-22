1c2849c996556eaed95ba8d7ce1c6a8847b18768
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-10-22 17:10:24 +0300

    Initial commit

---
+++ src/main/java/by/flameksandr/jpa_c1_e1/JpaC1E1Application.java
@@ -0,0 +1,13 @@
+package by.flameksandr.jpa_c1_e1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaC1E1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaC1E1Application.class, args);
+    }
+
+}
