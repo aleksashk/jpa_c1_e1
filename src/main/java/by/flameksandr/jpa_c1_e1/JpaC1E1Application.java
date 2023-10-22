549eeccdbcf13992785a8000d6d902d41d6b8aa5
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-10-22 17:16:52 +0300

    xml configuration with persistence.xml

--- src/main/java/by/flameksandr/jpa_c1_e1/JpaC1E1Application.java
+++ src/main/java/by/flameksandr/jpa_c1_e1/JpaC1E1Application.java
@@ -1,13 +1,24 @@
 package by.flameksandr.jpa_c1_e1;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 public class JpaC1E1Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(JpaC1E1Application.class, args);
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("my-persistence-unit");
+
+        EntityManager em = emf.createEntityManager();
+
+
+        try {
+            em.getTransaction().begin();
+            em.getTransaction().commit();
+        } finally {
+            em.close();
+        }
     }
 
 }
