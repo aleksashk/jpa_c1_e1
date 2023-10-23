32c6a6c8c38763638955c2aae470383479d96f33
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-10-23 17:50:24 +0300

    @ManyToMany cascading

--- src/main/java/by/flameksandr/jpa_c1_e1/entities/Group.java
+++ src/main/java/by/flameksandr/jpa_c1_e1/entities/Group.java
@@ -14,7 +14,7 @@ public class Group {
 
     private String name;
 
-    @ManyToMany
+    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.REMOVE})
     @JoinTable(
             name = "users_groups",
             joinColumns = @JoinColumn(name = "group_id"),
